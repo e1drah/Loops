ab4cb6e548332cbabb04358af801627fadaf76c5
Jacob Hardie <Hardiejacob22@gmail.com>
2022-10-31 09:49:29 -0300

    Started player controller

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -4,18 +4,52 @@ using System.Linq;
 using System.Media;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Loops
 {
     internal class Program
     {
         static bool gameover;
+
         static int playerY;
         static int playerX;
+
+        static char input;
         static void Main(string[] args)
         {
+            gameover = false;
+
+            PlayerUpdate();
         }
-        
+        static void PlayerUpdate()
+        {
+            while(gameover != true)
+            {
+                
+                if (Console.ReadKey().Key == ConsoleKey.W)
+                {
+                    Console.WriteLine("Up");
+                }
+                if (Console.ReadKey().Key == ConsoleKey.S)
+                {
+                    Console.WriteLine("Down");
+                }
+                if (Console.ReadKey().Key == ConsoleKey.A)
+                {
+                    Console.WriteLine("Left");
+                }
+                if (Console.ReadKey().Key == ConsoleKey.D)
+                {
+                    Console.WriteLine("Right");
+                }
+                if (Console.ReadKey().Key == ConsoleKey.Escape)
+                {
+                    gameover = true;
+                }
+            }
+            Environment.Exit(0);
+        }
 
     }
 }
